19569ec513dd4fc317c28c05a18cd771c40354d5
Danil Davydov <linadakill@gmail.com>
2020-04-28 05:02:19 -0700

    init

---
+++ StudentsList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentsList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
